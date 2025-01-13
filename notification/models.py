87c13d91e900261c533d0d8bc2d08d7d16082100
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2025-01-13 16:00:45 +0200

    Add notification app to the project
    With models:
     -NotificationCategory
     -NotificationTemplate
     -UserNotification
     -UserNotificationOption
     -UserNotificationSetting

---
+++ notification/models.py
@@ -0,0 +1,89 @@
+from django.conf import settings
+from django.db import models
+
+
+class NotificationCategory(models.Model):
+    name = models.CharField(max_length=32, blank=True, null=True)
+    title = models.CharField(max_length=32, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "notification_category"
+
+
+class NotificationTemplate(models.Model):
+    notification_category = models.ForeignKey(
+        NotificationCategory,
+        models.CASCADE,
+        related_name="notification_templates",
+    )
+    name = models.CharField(max_length=32, blank=True, null=True)
+    txt = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "notification_template"
+
+
+class UserNotification(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="user_notifications",
+    )
+    notification_template = models.ForeignKey(
+        NotificationTemplate,
+        models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="user_notifications",
+    )
+    notification_type = models.IntegerField()
+    status = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "user_notification"
+
+
+class UserNotificationOption(models.Model):
+    user_notification = models.ForeignKey(
+        UserNotification,
+        models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="user_notification_options",
+    )
+    field_id = models.IntegerField(blank=True, null=True)
+    txt = models.CharField(max_length=32, blank=True, null=True)
+
+    class Meta:
+        db_table = "user_notification_option"
+
+
+class UserNotificationSetting(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="user_notification_settings",
+    )
+    notification_template = models.ForeignKey(
+        NotificationTemplate,
+        models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="user_notification_settings",
+    )
+    system_notification = models.IntegerField()
+    push_notification = models.IntegerField()
+
+    class Meta:
+        db_table = 'user_notification_setting'
